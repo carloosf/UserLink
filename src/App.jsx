91262c8b3e19b1f63ae5e34af79c41db31f0bbfc
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-03-20 15:36:11 -0300

    Front Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,29 @@ import './App.css'
 
 function App() {
 
-  const user = [{
-    link: "https://www.linkedin.com/in/ccarlossilv/", namelink: "Linkedin",
-    link: "https://github.com/carloosf", namelink: "Github",
-    link: "https://www.instagram.com/ccarlos.exe/", namelink: "Instagram",
-    link: "mailto:contato.carlossilvaf@gmail.com", namelink: "Email",
-  }]
+  const user = [
+    { link: "https://www.linkedin.com/in/ccarlossilv/", namelink: "Linkedin" },
+    { link: "https://github.com/carloosf", namelink: "Github" },
+    { link: "https://www.instagram.com/ccarlos.exe/", namelink: "Instagram" },
+    { link: "mailto:contato.carlossilvaf@gmail.com", namelink: "Email" },
+  ]
 
   return (
 
     <body>
       <main>
+        <img src="src/assets/perfil.jpg" alt="" />
         <h2>Carlos Silva</h2>
-        <img src="./assets/perfil.jpg" alt="" />
 
         <ul>
           {user.map((item) => (
             <li>
-              <a 
-                href={item.link} 
+              <a
+                href={item.link}
                 target="_blank">
 
                 {item.namelink}
-                </a>
+              </a>
             </li>
           ))}
         </ul>
