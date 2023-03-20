d0d72c21a36a4644eab6c56994fbd6c7811df802
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-03-20 17:03:45 -0300

    get e post configurados

--- back/src/Routes.js
+++ back/src/Routes.js
@@ -1,28 +1,35 @@
 const { request, response } = require("express")
 const express = require("express")
 
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient()
-
-
-const allUsers = [{ nome: "" }]
+const allUsers = [
+    {
+        username: "carlos",
+        linkUser: [
+            { nameLink: "Instagram", link: "instagram.com/carloosf.__", status : false},
+            { nameLink: "GitHub", link: "github.com/carloosf", status : true},
+            { nameLink: "Linkedin", link: "linkedin.com/in/carloosf", status : true}
+        ]
+    }
+]
 const linkRoutes = express.Router()
 
 
 // C
 
-linkRoutes.post("/", async (request, response) => {
-    const { name } = request.body
-    const todo = await prisma.todo.create({
-        data: {
-            name,
-        },
-    })
-    return response.status(201).json(todo)
+linkRoutes.post("/post", (request, response) => {
+    const { username } = request.body
+    const newUser = {
+        username,
+    }
+    allUsers.push(newUser)
+    return response.status(201).json(allUsers)
 })
 
 // R
 
+linkRoutes.get("/get", (request, response) => {
+    return response.status(200).json(allUsers)
+})
 
 
 // U
--- back/src/Server.js
+++ back/src/Server.js
@@ -1,2 +1,12 @@
-const express = require('express')
+const express = require("express")
+const rotas = require("./Routes")
 const app = express()
+
+app.use(express.json())
+app.use(rotas)
+
+app.get("/health", (req, res) => {
+    return res.json("up")
+})
+
+app.listen(3333, () => console.log("Server up in 3333"))
