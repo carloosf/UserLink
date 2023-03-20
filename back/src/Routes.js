49632c12435a0b70712b0775d92eb2341d27409f
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-03-20 16:33:16 -0300

    reconfig

--- back/src/Routes.js
+++ back/src/Routes.js
@@ -5,13 +5,14 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
 
-const allTodos = [{ nome: "aaaa", status: false }]
-const todosRoutes = express.Router()
+const allUsers = [{ nome: "" }]
+
+const linkRoutes = express.Router()
 
 
 // C
 
-todosRoutes.post("/", async (request, response) => {
+linkRoutes.post("/", async (request, response) => {
     const { name } = request.body
     const todo = await prisma.todo.create({
         data: {
@@ -22,56 +23,14 @@ todosRoutes.post("/", async (request, response) => {
 })
 
 // R
-todosRoutes.get("/username", async (request, response) => {
-    const todos = await prisma.todo.findMany()
-    return response.status(200).json(todos)
-})
+
+
 
 // U
-todosRoutes.put("/username/edit", async (request, response) => {
-    const { name, id, status } = request.body
-    if (!id) {
-        return response.status(400).json("Id is mandatory")
-    }
 
-    const todoAlreadyExist = await prisma.todo.findUnique({ where: { id } })
-
-    if (!todoAlreadyExist) {
-        return response.status(404).json("Todo not exist")
-    }
-
-    const todo = await prisma.todo.update({
-        where: {
-            id,
-        },
-        data: {
-            name,
-            status
-        }
-    })
-
-    return response.status(200).json(todo)
-})
 
 // D
-todosRoutes.delete("/todos/:id", async (request, response) => {
-    const { id } = request.params
 
-    const intId = parseInt(id)
 
-    if (!intId) {
-        return response.status(400).json("Id is mandatory")
-    }
 
-    const todoAlreadyExist = await prisma.todo.findUnique({ where: { id: intId } })
-
-    if (!todoAlreadyExist) {
-        return response.status(404).json("Todo not exist")
-    }
-
-    await prisma.todo.delete({ where: { id: intId } })
-
-    return response.status(200).send()
-})
-
-module.exports = todosRoutes
+module.exports = linkRoutes
